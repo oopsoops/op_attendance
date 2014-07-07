b71c49c5c3a27d06cbf9e24082128e9cbe7c8fe1
oopsoops <paidaodi@foxmail.com>
2014-07-07 20:16:55 +0800

    添加boss  部门经理等角色在数据库
    添加boss  部门经理等角色在数据库，添加了菜单栏

--- disk/op_attendance.sql
+++ disk/op_attendance.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50516
 File Encoding         : 65001
 
-Date: 2014-07-04 18:16:37
+Date: 2014-07-07 20:15:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -182,13 +182,17 @@ CREATE TABLE `op_userinfo` (
   `email` varchar(255) DEFAULT NULL,
   `updatetime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of op_userinfo
 -- ----------------------------
-INSERT INTO op_userinfo VALUES ('1', '1001', 'test', '827ccb0eea8a706c4c34a16891f84e7b', '测试员工', '0', '1', null, '1', '2014-06-19', '1', null, null, '2014-06-19 14:06:50');
-INSERT INTO op_userinfo VALUES ('2', '1002', 'hr', '827ccb0eea8a706c4c34a16891f84e7b', '测试管理', '0', '2', null, '1', '2014-06-19', '2', null, null, '2014-06-19 23:29:00');
+INSERT INTO op_userinfo VALUES ('1', '1001', 'test', '827ccb0eea8a706c4c34a16891f84e7b', '产线员工', '0', '1', null, '1', '2014-06-19', '1', null, null, '2014-06-19 14:06:50');
+INSERT INTO op_userinfo VALUES ('2', '1002', 'hr', '827ccb0eea8a706c4c34a16891f84e7b', '人事经理', '0', '2', null, '1', '2014-06-19', '2', null, null, '2014-06-19 23:29:00');
+INSERT INTO op_userinfo VALUES ('3', '1003', 'monitor', '827ccb0eea8a706c4c34a16891f84e7b', '产线班长', '0', '1', null, '1', '2014-07-07', '3', null, null, '2014-07-12 17:38:26');
+INSERT INTO op_userinfo VALUES ('4', '1004', 'dpmanager', '827ccb0eea8a706c4c34a16891f84e7b', '部门经理', '0', '1', null, '1', '2014-07-01', '4', null, null, '2014-07-10 17:39:28');
+INSERT INTO op_userinfo VALUES ('5', '1005', 'boss', '827ccb0eea8a706c4c34a16891f84e7b', '老板', '0', '1', null, '1', '2014-06-04', '5', null, null, '2014-07-19 17:40:04');
+INSERT INTO op_userinfo VALUES ('6', '1006', 'admin', '827ccb0eea8a706c4c34a16891f84e7b', '管理员', '0', '1', null, '1', '2014-07-01', '6', null, null, '2014-07-19 17:40:37');
 
 -- ----------------------------
 -- Table structure for `op_usertype`
@@ -200,13 +204,17 @@ CREATE TABLE `op_usertype` (
   `menu` varchar(255) DEFAULT NULL,
   `power` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`tid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of op_usertype
 -- ----------------------------
 INSERT INTO op_usertype VALUES ('1', '员工', 'staff_menu.json', '1');
-INSERT INTO op_usertype VALUES ('2', '人力', 'hr_menu.json', '2');
+INSERT INTO op_usertype VALUES ('2', '人事经理', 'hr_menu.json', '2');
+INSERT INTO op_usertype VALUES ('3', '产线班长', 'monitor_menu.json', '3');
+INSERT INTO op_usertype VALUES ('4', '部门经理', 'dp_menu.json', '4');
+INSERT INTO op_usertype VALUES ('5', '老板', 'boss_menu.json', '5');
+INSERT INTO op_usertype VALUES ('6', '管理员', 'admin_menu.json', '6');
 
 -- ----------------------------
 -- Table structure for `op_worktime`
