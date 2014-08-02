ff64519e93c76a734280dd389245f12c00d41ce2
oopsoops <paidaodi@foxmail.com>
2014-08-02 16:35:02 +0800

    修改record_import  添加对象名称
    修改record_import  添加对象名称

--- Tpl/Hr/record_import.php
+++ Tpl/Hr/record_import.php
@@ -6,9 +6,9 @@
 		<tr>
 	    	<td><input name="import_xls" type="file" /></td>
 	        <td>开始日期：</td>
-	        <td><input id="import_begin_time" type="text" style="width:100px" /></td>
+	        <td><input name="import_begin_time"  id="import_begin_time" type="text" style="width:100px" /></td>
 	        <td>结束日期：</td>
-	        <td><input id="import_end_time" type="text" style="width:100px" /></td>
+	        <td><input name="import_end_time"   id="import_end_time" type="text" style="width:100px" /></td>
 	        <td><input type="submit"  value="导入" style="width:80px" /></td>
 	        <td><a class="easyui-linkbutton" onclick="import_clear()">清空</a></td>
 	        <td><a class="easyui-linkbutton" iconCls="icon-search" onclick="checkClock()">分析</a></td>
