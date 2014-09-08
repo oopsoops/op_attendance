80cafd19fdabaf7f6dc998da14a0bcfd1e4389f8
oopsoops <paidaodi@foxmail.com>
2014-09-08 10:43:25 +0800

    34
    434

--- Tpl/Works/transitionApply.php
+++ Tpl/Works/transitionApply.php
@@ -55,12 +55,46 @@
         结束时间：
         	<input type="text" id="trans_endtime2" class="easyui-timespinner"  value="17:30" />
         <br/><br />
+        休假类型：
+        <select id="holiday">
+        	<option value="">请选择</option>
+            <option value="年假">年假</option>
+            <option value="病假">病假</option>
+            <option value="婚假">婚假</option>
+            <option value="产假">产假</option>
+            
+        </select>
             <a class="easyui-linkbutton" iconCls="icon-search" onclick="apply2()">提交</a>
 	
 </div>
  
 <div title="出差申请"   style="padding:20px;"> 
-tab3 
+		<?php if ($teamlist[0]['teamid']!=1&&$power==3){ ?>
+        选择员工
+        <select id="uid3">
+            <?php for($i=0;$i<count($teamlist);$i++){
+                        echo "<option value='".$teamlist[$i]['uid']."'>".$teamlist[$i]['username'];	
+                  }
+            ?>	  
+        </select>&nbsp;&nbsp;&nbsp;&nbsp;
+        <?php }?>
+      
+        开始日期：
+            <input id="trans_begin_dat3e" type="text" style="width:100px" />
+        开始时间：
+        	<input type="text" id="trans_starttime3" class="easyui-timespinner"  value="08:30" />
+        结束日期：
+            <input id="trans_end_date3" type="text" style="width:100px" />
+        结束时间：
+        	<input type="text" id="trans_endtime3" class="easyui-timespinner"  value="17:30" />
+        <br/><br />
+        申请理由：<br />
+        	&nbsp;&nbsp;&nbsp;&nbsp; 
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <textarea id="reason" rows="8" cols="100"></textarea><br />
+            &nbsp;&nbsp;&nbsp;&nbsp; 
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a class="easyui-linkbutton" iconCls="icon-search" onclick="apply()">提交</a>
 </div>
 
 
@@ -137,8 +171,119 @@ tab3
 		});	
 	}
 	/************************************休假申请*************************************/
-	
-	
+	$('#trans_begin_date2').datebox({	
+			formatter:timeformatter,
+			parser:timeparser,
+			editable:false,
+			width: 100
+	});
+	$('#trans_end_date2').datebox({
+			formatter:timeformatter,
+			parser:timeparser,
+			editable:false,
+			width: 100		
+	});
+	function sendMEmail(content,emailAddress){}
+	function apply2(){
+		var transdm=$("#transName").val();
+		if(transdm==""){
+			$.messager.alert('提示','请选择事务类型！');
+			return false;
+		}
+		var begindate=$('#trans_begin_date2').combo("getValue");
+		var enddate=$("#trans_end_date2").combo("getValue");
+		var begintime=$('#trans_starttime2').val();
+		var endtime=$("#trans_endtime2").val();
+		if(begindate==""||enddate==""){
+			$.messager.alert("提示","请同时填写开始日期和结束日期！");
+			return false;
+		}
+		if(enddate<begindate){
+			$.messager.alert("提示","结束日期不能早于开始日期！");
+			return false;
+		}
+		if((enddate==begindate)&&(endtime<begintime)){
+			$.messager.alert("提示","同一天结束时间不能早于开始时间！");
+			return false;
+		}
+		var begin=begindate+" "+begintime;
+		var end=enddate+" "+endtime;
+		$.ajax({
+			url: "__APP__/Works/dealApply",
+			data:{
+				begindate:begindate,
+				enddate:enddate,
+				begintime:begintime,
+				endtime:endtime,
+				reason:reason,
+				transdm:transdm
+			},
+			type:'POST',
+			success:function(data){
+				if(data=="1"){
+					$.messager.alert("提示","提交成功！");
+				}				
+			},
+			error:function(XMLHttpRequest,textStatus,errorThrown){
+				alert(''+errorThrown)
+			}
+		});	
+	}
+	/************************************休假申请*************************************/
+	$('#trans_begin_date3').datebox({	
+			formatter:timeformatter,
+			parser:timeparser,
+			editable:false,
+			width: 100
+	});
+	$('#trans_end_date3').datebox({
+			formatter:timeformatter,
+			parser:timeparser,
+			editable:false,
+			width: 100		
+	});
+	function apply3(){
+		var transdm=3
+		
+		var begindate=$('#trans_begin_date3').combo("getValue");
+		var enddate=$("#trans_end_date3").combo("getValue");
+		var begintime=$('#trans_starttime3').val();
+		var endtime=$("#trans_endtime3").val();
+		if(begindate==""||enddate==""){
+			$.messager.alert("提示","请同时填写开始日期和结束日期！");
+			return false;
+		}
+		if(enddate<begindate){
+			$.messager.alert("提示","结束日期不能早于开始日期！");
+			return false;
+		}
+		if((enddate==begindate)&&(endtime<begintime)){
+			$.messager.alert("提示","同一天结束时间不能早于开始时间！");
+			return false;
+		}
+		var begin=begindate+" "+begintime;
+		var end=enddate+" "+endtime;
+		$.ajax({
+			url: "__APP__/Works/dealApply",
+			data:{
+				begindate:begindate,
+				enddate:enddate,
+				begintime:begintime,
+				endtime:endtime,
+				reason:reason,
+				transdm:transdm
+			},
+			type:'POST',
+			success:function(data){
+				if(data=="1"){
+					$.messager.alert("提示","提交成功！");
+				}				
+			},
+			error:function(XMLHttpRequest,textStatus,errorThrown){
+				alert(''+errorThrown)
+			}
+		});	
+	}
 	
 	
 	
