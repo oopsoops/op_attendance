10f985fbb32ab043c2f67b2c56c0cbd0ef599dcf
oopsoops <paidaodi@foxmail.com>
2014-11-12 22:37:45 +0800

    修改考勤导出格式和规则

--- Lib/Action/HrAction.class.php
+++ Lib/Action/HrAction.class.php
@@ -1820,7 +1820,7 @@ public function exportVacation()
 			
 					
 		
-			$where= "op_unusualtime.clockdate>='"."$export_begin_time"."'  and op_unusualtime.clockdate<='"."$export_end_time"."'  ";
+			$where= "op_unusualtime.standarddate>='"."$export_begin_time"."'  and op_unusualtime.standarddate<='"."$export_end_time"."'  ";
 		
 		   
           error_reporting(E_ALL);
@@ -1830,13 +1830,16 @@ public function exportVacation()
             $name=date('YmdHis');
 			
             $data=$clocktime->field("op_unusualtime.uid as uid,op_staffinfo.username as name,clockdate,standardtime,static,type,
+			op_vacationstatus.reason as reason,op_vacationstatus.begindate as bd,op_vacationstatus.enddate as ed,op_vacationstatus.begintime as bt,op_vacationstatus.endtime as et,
 			
 			CASE 
 			 WHEN  op_unusualtime.static = '正常' THEN  op_unusualtime.standardtime
 			 
 			 ELSE op_unusualtime.clocktime END AS clocktime
 			
-			")->join('op_staffinfo ON op_unusualtime.uid=op_staffinfo.uid')->where($where)->order('op_unusualtime.uid asc,op_unusualtime.clockdate asc,op_unusualtime.type asc')->select();
+			")->join('op_staffinfo ON op_unusualtime.uid=op_staffinfo.uid')
+			->join('op_vacationstatus ON op_unusualtime.vacid = op_vacationstatus.id')
+			->where($where)->order('op_unusualtime.uid asc, op_unusualtime.standarddate  asc,op_unusualtime.type asc')->select();
 			//echo $clocktime->getLastSql();
 			$flag=0;
 			$haveend=1; //表示已经下班
@@ -1854,7 +1857,7 @@ public function exportVacation()
 				{
 						if($haveend==0)  //之前考勤周期只有上班没有下班，就自动跳转到下一个考勤周期
 						{
-							$flag=$flag+1;
+						$flag=$flag+1;
 						$pdata[$flag]['uid']=$temp['uid'];
 						$pdata[$flag]['name']=$temp['name'];
 						$pdata[$flag]['begindate']=$temp['clockdate'];
@@ -1862,6 +1865,15 @@ public function exportVacation()
 						$pdata[$flag]['begintime']=$temp['clocktime'];
 						$pdata[$flag]['enddate']='0000-00-00';
 						$pdata[$flag]['endtime']='00:00:00';
+						$pdata[$flag]['statica']=$temp['static'];
+						$pdata[$flag]['staticb']='请到考勤系统具体查询';
+						$pdata[$flag]['bda']=$temp['bd'];
+						$pdata[$flag]['eda']=$temp['ed'];
+						$pdata[$flag]['bta']=$temp['bt'];
+						$pdata[$flag]['eta']=$temp['et'];
+						$pdata[$flag]['reasona']=$temp['reason'];
+						
+						
 						
 						}
 						else             //之前考勤记录已经下班，之前下班时已经跳转到了下一个周期
@@ -1871,7 +1883,18 @@ public function exportVacation()
 						$pdata[$flag]['begindate']=$temp['clockdate'];
 						
 						$pdata[$flag]['begintime']=$temp['clocktime'];
-							 $haveend=0;
+						$pdata[$flag]['statica']=$temp['static'];
+						$pdata[$flag]['bda']=$temp['bd'];
+						$pdata[$flag]['eda']=$temp['ed'];
+						$pdata[$flag]['bta']=$temp['bt'];
+						$pdata[$flag]['eta']=$temp['et'];
+						$pdata[$flag]['reasona']=$temp['reason'];
+						$pdata[$flag]['bdb']='';
+						$pdata[$flag]['edb']='';
+						$pdata[$flag]['btb']='';
+						$pdata[$flag]['etb']='';
+						$pdata[$flag]['reasonb']='';
+						$haveend=0;
 							}
 				
 				
@@ -1879,28 +1902,56 @@ public function exportVacation()
 				}
 				else if($temp['type']==1)  //如果是下班就保存下班时间作为下班时间日期，下一条数据就是下一个考勤周期的数据。
 				{
+							
+							
+
+							
 							//该下班记录是上一个员工上班记录的另一半
 							if($pdata[$flag]['uid']==$temp['uid'])
 							{
 							
 							$pdata[$flag]['enddate']=$temp['clockdate'];
 							$pdata[$flag]['endtime']=$temp['clocktime'];
+							$pdata[$flag]['staticb']=$temp['static'];
+							$pdata[$flag]['bdb']=$temp['bd'];
+							$pdata[$flag]['edb']=$temp['ed'];
+							$pdata[$flag]['btb']=$temp['bt'];
+							$pdata[$flag]['etb']=$temp['et'];
+							$pdata[$flag]['reasonb']=$temp['reason'];
 							$haveend=1;
 							$flag=$flag+1;
 							}
 							//是另外一个员工的下班时间
 							else
 							{
-							$pdata[$flag]['enddate']='0000-00-00';
-							$pdata[$flag]['endtime']='00:00:00';
-							$haveend=1;
-							$flag=$flag+1;
+							$pdata[$flag-1]['enddate']='0000-00-00';
+							$pdata[$flag-1]['endtime']='00:00:00';
+							$pdata[$flag-1]['staticb']='请到考勤系统具体查询';
+							$pdata[$flag-1]['bdb']='';
+							$pdata[$flag-1]['edb']='';
+							$pdata[$flag-1]['btb']='';
+							$pdata[$flag-1]['etb']='';
+							$pdata[$flag-1]['reasonb']='';
+
 							$pdata[$flag]['uid']=$temp['uid'];
 							$pdata[$flag]['name']=$temp['name'];
 							$pdata[$flag]['begindate']='0000-00-00';
 							$pdata[$flag]['begintime']='00:00:00';
 							$pdata[$flag]['enddate']=$temp['clockdate'];
 							$pdata[$flag]['endtime']=$temp['clocktime'];
+							$pdata[$flag]['statica']='请到考勤系统具体查询';
+							$pdata[$flag]['staticb']=$temp['static'];
+							
+							$pdata[$flag]['bda']='';
+							$pdata[$flag]['eda']='';
+							$pdata[$flag]['bta']='';
+							$pdata[$flag]['eta']='';
+							$pdata[$flag]['reasona']='';
+							$pdata[$flag]['bdb']=$temp['bd'];
+							$pdata[$flag]['edb']=$temp['ed'];
+							$pdata[$flag]['btb']=$temp['bt'];
+							$pdata[$flag]['etb']=$temp['et'];
+							$pdata[$flag]['reasonb']=$temp['reason'];
 								
 								
 								}
@@ -1929,9 +1980,33 @@ public function exportVacation()
                                 ->setKeywords("excel")
                                ->setCategory("result file");
           /*以下就是对处理Excel里的数据， 横着取数据，主要是这一步，其他基本都不要改*/
+		  $a=1;
+		  $objPHPExcel->setActiveSheetIndex(0)
+ 
+                         //Excel的第A列，uid是你查出数组的键值，下面以此类推
+                           ->setCellValue('A'.$a, 'ID')    
+                           ->setCellValue('B'.$a, '姓名')
+                           ->setCellValue('C'.$a,  '上班打卡日期')
+                            ->setCellValue('D'.$a, '下班打卡日期')
+                             ->setCellValue('E'.$a, '上班打卡时间')
+                               ->setCellValue('F'.$a, '下班打卡时间')
+							    ->setCellValue('G'.$a, '上班打卡状态')
+								 ->setCellValue('H'.$a, '上班打卡异常原因')
+								 ->setCellValue('I'.$a, '上班请假开始日期')
+								  ->setCellValue('J'.$a, '上班请假开始时间')
+								   ->setCellValue('K'.$a, '上班请假结束日期')
+								    ->setCellValue('L'.$a, '上班请假结束时间')
+									 ->setCellValue('M'.$a, '下班打卡状态')
+									 ->setCellValue('N'.$a, '下班打卡异常原因')
+										->setCellValue('O'.$a, '下班请假开始日期')
+								 		 ->setCellValue('P'.$a, '下班请假开始时间')
+								  			 ->setCellValue('Q'.$a, '下班请假结束日期')
+								  			  ->setCellValue('R'.$a, '下班请假结束时间');
+             
+		  
          foreach($pdata as $k => $v){
  
-             $num=$k+1;
+             $num=$k+2;
               $objPHPExcel->setActiveSheetIndex(0)
  
                          //Excel的第A列，uid是你查出数组的键值，下面以此类推
@@ -1940,7 +2015,19 @@ public function exportVacation()
                            ->setCellValue('C'.$num, $v['begindate'])
                             ->setCellValue('D'.$num, $v['enddate'])
                              ->setCellValue('E'.$num, $v['begintime'])
-                               ->setCellValue('F'.$num, $v['endtime']);
+                               ->setCellValue('F'.$num, $v['endtime'])
+							    ->setCellValue('G'.$num, $v['statica'])
+								 ->setCellValue('H'.$num, $v['reasona'])
+								 ->setCellValue('I'.$num, $v['bda'])
+								 ->setCellValue('J'.$num, $v['bta'])
+								 ->setCellValue('K'.$num, $v['eda'])
+								 ->setCellValue('L'.$num, $v['eta'])
+								 ->setCellValue('M'.$num, $v['staticb'])
+								 ->setCellValue('N'.$num, $v['reasonb'])
+								 ->setCellValue('O'.$num, $v['bdb'])
+								 ->setCellValue('P'.$num, $v['btb'])
+								 ->setCellValue('Q'.$num, $v['edb'])
+								 ->setCellValue('R'.$num, $v['etb']);
              }
  
             $objPHPExcel->getActiveSheet()->setTitle('报表');
